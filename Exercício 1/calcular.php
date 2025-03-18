797e990606e7d0dd1596c200891ff2667a58c975
Filipe-Sbrici <filipesbrici@gmail.com>
2025-03-18 19:08:43 -0300

    f 0.8

--- Exercício 1/calcular.php
+++ Exercício 1/calcular.php
@@ -31,28 +31,31 @@
         {
             $xbu = $xbu / 6;
             echo "você comeu $xbu X-burguers" 
-        }
+        }else{}
 
         if ($xsa != 0)
         {
             $xsa = $xsa / 8;
             echo "você comeu $xsa X-saladas" 
-        }
+        }else{}
+
         if ($xba != 0)
         {
             $xba = $xba / 10;
             echo "você comeu $xba X-bacons" 
-        }
+        }else{}
+
         if ($xtu != 0)
         {
             $xtu = $xtu / 12;
             echo "você comeu $xtu X-tudo" 
-        }
+        }else{}
+        
         if ($xdu != 0)
         {
             $xdu = $xdu / 15;
             echo "você comeu $xdu X-duplos" 
-        }
+        }else{}
 
 
 
